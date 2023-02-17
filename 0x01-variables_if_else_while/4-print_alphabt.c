6ba9f29fb9b6eaf6675316dee3ca65cf285354b7
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-17 06:23:16 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 on success, other values on error
+ */
+int main(void)
+{
+	int small;
+
+	for (small = 97; small < 123; small++)
+	{
+		char small_char = (char) small;
+
+		if (small != 101 || small != 113)
+		{
+			putchar (small_char);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
