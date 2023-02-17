0bfefd7c72b1f28830ca7ae171bbfe2fc1973a1f
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-17 11:32:51 -0800

    01-print_comb4.c

---
+++ 0x01-variables_if_else_while/01-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 on success, other values on error
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (k != 7)
+				{
+					putchar(43 + 1);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
