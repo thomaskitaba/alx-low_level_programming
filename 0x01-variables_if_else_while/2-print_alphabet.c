ee7771630b1d56d5dbb101eaa5769bcd53fb74d7
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-17 06:15:56 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 on success, other values on error
+ */
+int main(void)
+{
+	int small;
+	int cap;
+
+	for (small = 97; small < 123; small++)
+	{
+		char small_char = (char) small;
+
+		putchar (small_char);
+	}
+	putchar('\n');
+	return (0);
+}
