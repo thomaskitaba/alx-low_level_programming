31407d1161e49845c7362a5881ba6ce87da0f973
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-17 12:48:25 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 on success, other values on error
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i; j < 10; j++)
+		{
+			for (k = j; k < 10; k++)
+			{
+				for (l = j + 1; l < 10; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(44);
+					putchar('0' + k);
+					if (j != 8)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
