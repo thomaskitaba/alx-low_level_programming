7c5588fa06bc5e6ba228087cf7c364e036a00ee1
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-20 09:48:48 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 
         for (k = 0; k < 10; k++)
         {
-                for (m = 0; m < 10; m++)
+                for (m = 0; m < 9; m++)
                 {
                         for (i = 0; i < 10; i++)
                         {
-				for (j = 0; j < 9; j++)
+				for (j = 0; j < 10; j++)
 				{
 					if ( j !=0 || i != 0)
 					{
@@ -27,7 +27,7 @@ int main(void)
 						putchar(32);
 						putchar('0' + i);
 						putchar('0' + j);
-						if (i != 9 || j !=8)
+						if (i != 9 || j !=9)
 						{
 							putchar(44);
 							putchar(32);
