83f6884c842f5d114cd3e65d8ae02361d7eab796
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-20 09:55:40 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -34,11 +34,9 @@ int main(void)
 						}
 					}
 				}
-
-                                }
-                        }
-                }
-        }
+			}
+		}
+	}
         putchar('\n');
         return (0);
-
+}
