87d09f1cdd6f53bf472806e27cadbd91cb1cdda6
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 05:58:16 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,14 +8,6 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		_putchar('i');
-		_putchar('s');
-		_putchar('l');
-		_putchar('o');
-		_putchar('w');
-		_putchar('e');
-		_putchar('r');
-		_putchar('\n');
 		return (1);
 	}
 	else
