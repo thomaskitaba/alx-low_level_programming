3730032a0107cc086062fc7c4519b63d2198ea9a
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 12:45:10 -0800

    fibonic 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main: entry for the fibonic series
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(void)
+{
+	int temp = 1;
+	int num2 = 2;
+	int fib = 0;
+	int count = 0;
+	
+	while (count < 50)
+	{
+		if (count == 0)
+			printf("%d", temp);
+
+		else if (count == 1)
+			pdrintf("%d", num2);
+		else
+		{
+			fib = temp + num2;
+			temp = num2;
+			num2 = fib;
+			printf("%d", fib);
+		}
+		if (count != 49)
+			printf(", ");
+		count += 1;
+	}
+	return (0);
+}
+
