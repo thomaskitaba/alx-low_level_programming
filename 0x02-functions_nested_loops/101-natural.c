543a4ed5e50da20f328766c5f17c03c47ca25ece
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 08:37:38 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,13 +11,13 @@ int main(void)
 	int n;
 	int sum;
 
-	for(n = 0; n < 1024; n++)
+	for (n = 0; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
 		{
 			sum += n;
 		}
 	}
-	printf("%ld", sum);
+	printf("%d", sum);
 	return (0);
 }
