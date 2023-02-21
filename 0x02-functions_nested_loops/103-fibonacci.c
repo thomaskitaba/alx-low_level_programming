dc35866dd154649d33d5e52a4af207b331bfc89a
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 13:30:25 -0800

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * main: entry for the fibonic series
+ * main - entry for the fibonic series
  *
  * Return: 0 on success, 1 on error
  */
 int main(void)
 {
-	int temp = 1;
-	int num2 = 2;
-	int fib = 0;
+	long int temp = 1;
+	long int num2 = 2;
+	long int fib = 0;
 	int count = 0;
-	
-	while (count < 50)
-	{
-		if (count == 0)
-			printf("%d", temp);
+	long sum = 2;
 
-		else if (count == 1)
-			pdrintf("%d", num2);
-		else
-		{
-			fib = temp + num2;
-			temp = num2;
-			num2 = fib;
-			printf("%d", fib);
-		}
-		if (count != 49)
-			printf(", ");
+	while (fib <= 4000000)
+	{
+		fib = temp + num2;
+		temp = num2;
+		num2 = fib;
+		if (fib % 2 == 0)
+			sum += fib;
 		count += 1;
 	}
+	printf("%ld\n", sum);
 	return (0);
 }
-
