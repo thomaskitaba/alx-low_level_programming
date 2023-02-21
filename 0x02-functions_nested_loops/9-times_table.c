a6435e0d9056aa033e9bc7711b5b81d28312294a
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 10:17:29 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,32 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int f;
+	int l;
+	int result;
 
 	for (i = 0; i < 10; i++)
 	{
-		int result = 0;
 
 		for (j = 0; j < 10; j++)
 		{
 			result = i * j;
-			_putchar(result);
+			if (result > 9)
+			{
+				l = result % 10;
+				f = (result - l) / 10;
+
+				_putchar('0' + f);
+				_putchar('0' + l);
+			}
+			else
+			{
+				_putchar('0' + result);
+			}
 			if (i != 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(44);
+				_putchar(32);
 			}
 		}
 		_putchar('\n');
