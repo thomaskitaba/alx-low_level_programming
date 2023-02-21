eda92028ebf4e9031ec2dd04248bebb63496418d
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 06:35:41 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - calculate the absolute value of a number
+ * @n:  the character to be checked
+ * Return: 0 on if lowercase, 0 if uppercase
+ */
+int _abs(int n)
+{
+	char a = abs(n);
+
+	_putchar(a);
+	return (0);
+}
