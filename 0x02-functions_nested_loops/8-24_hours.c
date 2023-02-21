7ffef641b5e605f48ba5da5146980f573cb61d71
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 11:57:12 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,12 +13,16 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 5; k++)
 			{
 				for (l = 0; l <= 9; l++)
 				{
+					if ( i == 2 && j == 3 && k == 5 && l == 9)
+					{
+						exit(0);
+					}
 					_putchar('0' + i);
 					_putchar('0' + j);
 					_putchar(':');
