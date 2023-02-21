3e1c636a2be0f850968288ff1870eb7fbbb4cfd8
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-21 05:16:55 -0800

    0-putchar

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,12 @@
-#include  "main.h"
+#include "main.h"
 #include <unistd.h>
+
 /**
- *  _putchar - writes the character c to stdout
- *  @c: The character to print
- *
- *  Return: On success 1.
- *  On error, -1 is returned, and errno is set appropriately
+ * _putchar -  writes the character c to stdout
+ * @c: The character to print
+ * Return: return 0 on success
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifdef main_h
+#ifndef main_h
 #define main_h
 
-int _putchar(void);
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
