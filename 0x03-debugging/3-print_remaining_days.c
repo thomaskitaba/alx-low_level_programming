9df0fac8f614dbafe67dac7f805f1bad63399c76
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-22 12:32:06 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,11 +18,10 @@ if (month >= 2 && day >= 60)
 {
 day++;
 }
-
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
-else
+else if (year % 4 != 0 && year % 100 == 0)
 {
 if (month == 2 && day == 60)
 {
