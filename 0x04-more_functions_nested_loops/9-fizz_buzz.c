d8788872e937cd5c283af4e1c89bc96c2b23db19
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 08:36:06 -0800

    9-fizbuz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - entry for fizz buzz
  *
@@ -6,31 +7,39 @@
  */
 int main(void)
 {
-	int i;
+        int i, f, l;
 
-	for (i = 1; i < 101; i++)
-	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-			continue;
-		}
-		if (i % 3 == 0)
-		{
-			printf("Fizz");
-			continue;
-		}
-		if (i % 5 == 0)
-		{
-			if (i == 100)
-				printf("Buzz");
-			else
-				printf("Buzz ";
-			continue;
-		}
-		printf("%d ", i);
+        for (i = 1; i < 101; i++)
+        {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                        _putchar('F'), _putchar('i'), _putchar('z'), _putchar('z');
+                        _putchar('B'), _putchar('u'), _putchar('z');
+                        _putchar('z');
+                        _putchar(32);
+                        continue;
+                }
+                if (i % 3 == 0)
+                {
+                        _putchar('F'), _putchar('i');
+                        _putchar('z'), _putchar('z');
+                        _putchar(32);
+                        continue;
+                }
+                if (i % 5 == 0)
+                {
 
-	}
-	printf("\n");
-	return (0);
+                        _putchar('B'), _putchar('u');
+                        _putchar('z'), _putchar('z');
+                        _putchar(32);
+                        continue;
+                }
+
+                l = i % 10;
+                f = (i - l) / 10;
+                if (i > 9)
+                        _putchar('0' + f);
+                _putchar('0' + l), _putchar(32);
+        }
+        return (0);
 }
