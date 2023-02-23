54369a7fe8f6da05adb22817663cf6b76b8c650a
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 04:10:33 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 upto 14    x10
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int f;
+	int l;
+
+	for (i = 0; i < 14; i++)
+	{
+		l = i % 10;
+		f = (i - l) / 10;
+
+		if (i > 10)
+		{
+			_putchar('0' + f);
+		}
+		_putchar('0' + l);
+	}
+	_putchar('\n');
+}
