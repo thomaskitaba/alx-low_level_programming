8a51cdc94413f766595963327ea5f7afb9ba6980
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 05:00:12 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 upto 14 x10
+ * @size: size of square
+ */
+void print_square(int size);
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#' + l);
+			}
+			_putchar('\n');
+		}
+	}
+}
