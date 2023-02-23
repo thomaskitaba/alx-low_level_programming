c0d5f306159248a0ce188ab8dd4ffe44c6c6e07a
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 04:01:12 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * print_numbers - print 0 upto 9
+ * print_numbers - print 0 to 9
  *
- * Return: 0 up 9
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		_putchar('0' + i);
 	}
