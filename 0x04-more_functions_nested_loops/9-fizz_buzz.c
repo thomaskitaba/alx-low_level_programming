f92411cafffb5ac557f51bc50d81cef33fec2a3b
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 08:44:20 -0800

    9-Fizz_Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,39 +7,39 @@
  */
 int main(void)
 {
-        int i, f, l;
+int i, f, l;
 
-        for (i = 1; i < 101; i++)
-        {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                        _putchar('F'), _putchar('i'), _putchar('z'), _putchar('z');
-                        _putchar('B'), _putchar('u'), _putchar('z');
-                        _putchar('z');
-                        _putchar(32);
-                        continue;
-                }
-                if (i % 3 == 0)
-                {
-                        _putchar('F'), _putchar('i');
-                        _putchar('z'), _putchar('z');
-                        _putchar(32);
-                        continue;
-                }
-                if (i % 5 == 0)
-                {
-
-                        _putchar('B'), _putchar('u');
-                        _putchar('z'), _putchar('z');
-                        _putchar(32);
-                        continue;
-                }
-
-                l = i % 10;
-                f = (i - l) / 10;
-                if (i > 9)
-                        _putchar('0' + f);
-                _putchar('0' + l), _putchar(32);
-        }
-        return (0);
+for (i = 1; i < 101; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+_putchar('F'), _putchar('i'), _putchar('z'), _putchar('z');
+_putchar('B'), _putchar('u'), _putchar('z');
+_putchar('z');
+_putchar(32);
+continue;
+}
+if (i % 3 == 0)
+{
+_putchar('F'), _putchar('i');
+_putchar('z'), _putchar('z');
+_putchar(32);
+continue;
+}
+if (i % 5 == 0)
+{
+_putchar('B'), _putchar('u');
+_putchar('z'), _putchar('z');
+if (i != 100)
+_putchar(32);
+continue;
+}
+l = i % 10;
+f = (i - l) / 10;
+if (i > 9)
+_putchar('0' + f);
+_putchar('0' + l), _putchar(32);
+}
+_putchar('\n');
+return (0);
 }
