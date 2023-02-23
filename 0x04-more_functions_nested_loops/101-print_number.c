c5cb87078378eaeb90bdde1c3305b021ec9616f4
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 10:58:17 -0800

    void print_number(int n)

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * print_number - print up to 4 digit number
+ * @n: number to be printed
+ * Return: nothing
+ */
+void print_number(int n)
+{
+int n = 422;
+int f, t, l, s, temp;
+if (n == 0)
+{
+_putchar('0' + n);
+}
+if (n > 9 && n < 100)
+{
+l = n % 10;
+f = (n - l) / 10;
+_putchar('0' + f), _putchar('0' + l);
+}
+if (n >= -99 && n < 0)
+{
+n = -(n);
+l = n % 10;
+f = (n - l) / 10;
+_putchar('-'), _putchar('0' + f), _putchar('0' + l);
+}
+if (n > 99 && n < 1000)
+{
+l = n % 10;
+temp = (n - l) / 10;
+s = temp % 10;
+f = (temp - s) / 10;
+_putchar('0' + f), _putchar('0' + s), _putchar('0' + l);
+}
+if (n > 999 && n < 10000)
+{
+l = n % 10;
+temp = (n - l) / 10;
+t = temp % 10;
+temp = (temp - t) / 10;
+s = temp % 10;
+f = (temp - s) / 10;
+_putchar('0' + f), _putchar('0' + s), _putchar('0' + t), _putchar('0' + l);
+}
+}
