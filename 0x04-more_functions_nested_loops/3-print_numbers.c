53f0963bb2c79c1d92ace8c3c2594e02a9feebef
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 02:48:52 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_numbers - checks for upper case
+ * print_numbers - print 0 to 9 followed by a new line
  *
  */
 void print_numbers(void)
 {
-	int i;
+	int;
 
 	for (i = 48; i < 58; i++)
 	{
