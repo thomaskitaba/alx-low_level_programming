19cafbefe732c4279bf0b17f5c99dba465b50886
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 06:18:08 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry for fizz buzz
+ *
+ * Return: 0 on success, other integer on error
+ */
+int main(void)
+{
+	int i, f, l;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar('F'), _putchar('i'), _putchar('z'), _putchar('z');
+			_putchar('B'), _putchar('u'), _putchar('z');
+			_putchar('z');
+			_putchar(32);
+			continue;
+		}
+		if (i % 3 == 0)
+		{
+			_putchar('F'), _putchar('i');
+			_putchar('z'), _putchar('z');
+			_putchar(32);
+			continue;
+		}
+		if (i % 5 == 0)
+		{
+
+			_putchar('B'), _putchar('u');
+			_putchar('z'), _putchar('z');
+			_putchar(32);
+			continue;
+		}
+
+		l = i % 10;
+		f = (i - l) / 10;
+		if (i > 9)
+			_putchar('0' + f);
+		_putchar('0' + l), _putchar(32);
+	}
+	return (0);
+}
