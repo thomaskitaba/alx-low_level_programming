907aa464a7d8c600e3e6e09d771574790e856b59
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 02:26:37 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - checks for upper case
+ * @a: number 1 to be added
+ * @b: number 2 to be added
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
