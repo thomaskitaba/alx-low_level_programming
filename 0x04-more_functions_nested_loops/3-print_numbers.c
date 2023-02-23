6af29d3c296ee8eaad719a35646bb7230d24459b
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 05:24:17 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,13 @@
 /**
  * print_numbers - print 0 to 9
  *
+ *
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		_putchar('0' + i);
 	}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - print 0 upto 14 x10
+ * @size: size of square
+ *
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
