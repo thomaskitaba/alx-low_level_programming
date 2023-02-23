e6868b170e9401503998d5f987274565225f7bbd
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 10:00:42 -0800

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - entry point for greatest prime factor
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(void)
+{
+int i, j;
+long long prime_factor = 612852475143;
+int n = sqrt(prime_factor);
+for (i = 2; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (prime_factor % i == 0 && prime_factor != i)
+{
+prime_factor = prime_factor / i;
+}
+else
+break;
+}
+}
+printf("%lld\n", prime_factor);
+return (0);
+}
