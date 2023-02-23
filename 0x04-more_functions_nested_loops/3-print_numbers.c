d6e73c22a4ffb41224464e24da03862673bc2c66
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-23 02:39:47 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - checks for upper case
+ *
+ * Return: product
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
