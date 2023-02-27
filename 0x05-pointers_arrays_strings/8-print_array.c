44e473b05f152ee0fb772d18d53c56df0d58fc50
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-27 11:03:48 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,9 +3,15 @@
  * print_array - checks for upper case
  * @a: array in question
  * @n: number of array elements
- * 
+ * Return: nothing
  */
 void print_array(int *a, int n)
 {
+int i;
 
+for (i = 0; i < n; i++)
+{
+_putchar(a[i]);
+}
+_putchar('\n');
 }
