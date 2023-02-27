d16eeee84e7b3cf7d622e753efb25c9ef5c334c9
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-27 10:33:18 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,11 +7,10 @@
  */
 void _puts(char *str)
 {
-	int i;
-	for (i = 0; i != (int)strlen(str); i++)
-	{
-
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i != (int)strlen(str); i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
