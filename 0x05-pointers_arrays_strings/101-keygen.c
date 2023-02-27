1dea8f08679fed0200045c41651148da2cff6ed4
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-27 14:09:05 -0800

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,28 +3,28 @@
 #include <time.h>
 #include <string.h>
 
-#define PASSWORD_LENGTH 10
 
-char* generate_password();
+char* generate_password(int p);
 
 int main() {
-    char* password = generate_password();
-    printf("Generated password: %s\n", password);
-    free(password);
-    return 0;
+int password_length = 10;    
+char* password = generate_password(password_length);
+printf("Generated password: %s\n", password);
+free(password);
+   return 0;
 }
 
-char* generate_password() {
+char* generate_password(int p) {
     srand(time(NULL));
-    char* password = malloc((PASSWORD_LENGTH + 1) * sizeof(char));
+    char* password = malloc((p + 1) * sizeof(char));
     if (password == NULL) {
         fprintf(stderr, "Error: memory allocation failed\n");
         exit(1);
     }
     const char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}\\|;:'\",./<>?";
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
+    for (int i = 0; i < p; i++) {
         password[i] = valid_chars[rand() % strlen(valid_chars)];
     }
-    password[PASSWORD_LENGTH] = '\0';
+    password[p] = '\0';
     return password;
 }
