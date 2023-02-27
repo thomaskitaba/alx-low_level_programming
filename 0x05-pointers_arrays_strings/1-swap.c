bc65278f39c942979a1bba2f0893bc5af663143b
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-27 14:25:44 +0300

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,12 @@
  * swap_int - swaps string varibles
  * @a: char to be swaped
  * @b: char to be swaped
+ * Return: nothing
  */
 void swap_int(int *a, int *b)
 {
-
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
