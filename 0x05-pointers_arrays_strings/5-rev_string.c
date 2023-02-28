7d39a3d32729fdd2f9979d2e36b4ac7b03287f80
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-28 02:57:01 -0800

    5-reverse_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,14 +3,20 @@
 /**
  * rev_string - reverse string
  * @s: string to be reversed
- * 
+ * Return: nothing
  */
 
 void rev_string(char *s)
 {
-	int i;
-	for (i = strlen(s) - 1; i >= 0; i--)
+	int i, middle, len;
+	char temp;
+
+	len = (int)strlen(s);
+	middle = len / 2;
+	for (i = 0; i < middle; i++)
 	{
-		_putchar(s[i]);
+		temp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = temp;
 	}
 }
