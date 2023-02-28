b26176c550e7642cc52745c30aa320d1649af17c
thomaskitaba <thomas.kitaba@gmail.com>
2023-02-28 06:15:39 -0800

    8-reverse_integer

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,25 +4,50 @@
  * print_array - checks for upper case
  * @a: array in question
  * @n: number of array elements
- * Return: nothing
  */
 void print_array(int *a, int n)
 {
-int i;
-int j;
+int i, j, k, num, f, sign, temp, int_len, power;
 
+sign = 1, int_len = 0;
 for (i = 0; i < n; i++)
 {
-	for (j = 0; j < (int)strlen(a[i]); j++)
-	{
-		_putchar(a[i][j]);
-	}
-	_putchar(',');
-	if (i != n - 1)
-	{
-		_putchar(' ');
-	}
+if (a[i] < 0)
+{
+sign = -1, temp = -(a[i]);
 }
-_putchar('\n');
+else
+{
+sign = 1, temp = a[i];
 }
+num = temp, int_len = 0;
+while (temp > 0)
+{
+temp /= 10, int_len++;
+}
+if (num != 0)
+{
+for (j = 0; j < int_len; j++)
+{
+power = 1;
+for (k = j; k < int_len - 1; k++ )
+{
+power *= 10;
+(
+f = num / power, num -= f * power;
+if (j == 0 && sign < 0)
+putchar('-')
+putchar('0' + f);
+}
+}
+else
+putchar('0');
+if (i + 1 != n)
+{
+putchar(','), putchar(' ');
+}
+}
+}
+
+
 
