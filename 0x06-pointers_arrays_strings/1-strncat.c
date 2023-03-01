58c448ced537fa096edf84a19ade61cf21d12f89
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-01 16:07:55 +0300

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _strncat - swaps string varibles
  * @src: source
@@ -8,5 +9,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  
+  int i, dest_len, src_len;
+dest_len = (int)strlen(dest);
+src_len = (int)strlen(src);
+
+/* first find the null char*/
+if (n > src_len)
+{
+  n = src_len;
+}
+for (i = 0; i < n; i++)
+{
+dest[dest_len + i] = src[i];
+if (i + 1 == src_len)
+{
+	dest[dest_len + i + 1] = '\0';
+}
+}
+return (dest);
 }
