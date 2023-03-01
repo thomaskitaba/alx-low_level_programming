2bdab40933eafe8ede0f6473ba2b58075e83769a
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-01 19:18:34 +0300

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,22 @@
 #include "main.h"
+#include <string.h>
 /**
  * string_toupper - swaps string varibles
- * @: char to be swaped
- * @b: char to be swaped
+ * @str: char to be upper cased
+ *
  * Return: nothing
  */
 char *string_toupper(char * str)
 {
+	int i, str_len;
 
+	str_len = (int)strlen(str);
+	for (i = 0; i < str_len; i++)
+	{
+		if(str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
 }
