c8eda4a1603a41d306ea71c42e8e190f86ca119b
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-01 17:12:23 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,40 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	
-	return (0);
+
+int i, s1_len, s2_len, result;
+
+s1_len = (int)strlen(s1);
+s2_len = (int)strlen(s2);
+
+for (i = 0; i < s1_len; i++)
+{
+if (s1[i] == s2[i])
+{
+result = 0;
+}
+
+else
+{
+if ((s1[i] >= 97 && s1[i] <= 122) && (s1[i] >= 65 && s2[i] <= 90) && (s1[i] - s2[i]))
+{
+result = s2[i] - s1[i];
+return (result);
+}
+
+result = s1[i] - s2[i];
+return (result);
+}
+
+}
+if (i + 1 == s1_len)
+{
+if (s1_len < s2_len)
+{
+result = 0 - s2[i];
+return (result);
+}
+}
+return (0);
+
 }
