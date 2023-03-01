ca6b3be8eb3986688f038d5efa336c398b67b847
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-01 19:09:55 +0300

    4-reverse_array.c

--- 0x06-pointers_arrays_strings/4-rev_array copy.c
+++ 0x06-pointers_arrays_strings/4-rev_array copy.c
@@ -7,13 +7,14 @@
  */
 void reverse_array(int *a, int n)
 {
-int i, middle, temp;
+	int i, middle, temp;
 
-middle = n / 2;
-for (i = 0; i < middle; i++)
-{
-temp = a[i];
-a[i] = a[n - 1 - i];
-a[n -1 - i] = temp;
-}
+	middle = n / 2;
+	for (i = 0; i < middle; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n -1 - i] = temp;
+	}
+
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,11 @@
 #include "main.h"
 /**
- * swap_int - swaps string varibles
- * @a: char to be swaped
+ * string_toupper - swaps string varibles
+ * @: char to be swaped
  * @b: char to be swaped
  * Return: nothing
  */
+char *string_toupper(char * str)
+{
+
+}
