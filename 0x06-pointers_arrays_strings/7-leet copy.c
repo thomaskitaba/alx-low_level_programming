ad040a09526b5d1ad8a71cebe82b68239607eedb
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-02 11:20:07 +0300

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet copy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * leet - swaps string varibles
+ * @str: char to be swaped
+ * Return: nothing
+ */
+char *leet(char *str)
+{
+int i, j, str_len, ltr_len;
+
+char letter[]= "aAeEoOtTlL";
+char number[]= "4433007711";
+str_len = (int)strlen(str);
+ltr_len = (int)strlen(letter);
+
+for (i = 0; i < str_len; i++)
+{
+for (j = 0; j < ltr_len; j++)
+{
+if (str[i] == letter[j])
+{
+str[i] = number[j];
+}
+}
+}
+return (str);
+}
