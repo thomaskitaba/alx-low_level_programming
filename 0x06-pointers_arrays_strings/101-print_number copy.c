9ec35b0508d4e14990becee604de1fd356e31ba6
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-02 13:54:42 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number copy.c
+++ 0x06-pointers_arrays_strings/101-print_number copy.c
@@ -3,38 +3,38 @@
 /**
  * print_number - print integers
  * @n: number to be printed
- * Return: nothing 
+ * Return: nothing
  */
 void print_number(int n)
 {
-  int i, temp, k, int_len, power, f;
+int j, k, num, f_power, f, sign, temp, int_len, power;
 
-  int_len = 0, temp = n, power = 1;
+sign = 1, int_len = 0;
 
-  if (n < 0)
-  {
-    n *= -1;
-    temp = n;
-    _putchar('-');
-  }
-  /*find the length of the integer*/
-  while(temp > 0)
-  {
-    temp /= 10;
-    int_len++;
-  }
+if (n < 0)
+	sign = -1, temp = -(n);
+else
+	sign = 1, temp = n;
 
-  for(i = 0; i < int_len; i++)
-  {
-    /* find power*/
-    power = 1;
-    for (k = i; k < int_len - 1; k++)
-    {
-      power *= 10;
-    }
-    f = temp / power;
-    temp -= (f * power); 
-    _putchar('0' + f);
-    
-  }
+num = temp, int_len = 0;
+
+while (temp > 0)
+{ temp /= 10, int_len++; }
+
+if (num != 0)
+{
+	for (j = 0; j < int_len; j++)
+	{
+	power = 1;
+	for (k = j; k < int_len - 1; k++)
+	{ power *= 10; }
+	f = num / power, f_power = f *power, num -= f_power;
+	if (j == 0 && sign < 0)
+		_putchar('-');
+	_putchar('0' + f);
+	}
+}
+else
+	_putchar('0');
+_putchar('\n');
 }
