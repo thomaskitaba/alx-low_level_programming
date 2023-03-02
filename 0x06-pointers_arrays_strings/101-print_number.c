6939ffcec2685d48f5e89bb0bffc2e2bcbff9ccc
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-02 12:49:50 +0300

    101-print_numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include 'main.h'
+#include <string.h>
+void print_number(int n)
+{
+  int i, j, k, num, f_power, f, sign, temp, int_len, power;
+
+sign = 1, int_len = 0;
+if (n < 0)
+sign = -1, temp = -(n);
+else
+sign = 1, temp = n;
+num = temp, int_len = 0;
+while (temp > 0)
+{ temp /= 10, int_len++; }
+if (num != 0)
+{
+for (j = 0; j < int_len; j++)
+{
+power = 1;
+for (k = j; k < int_len - 1; k++)
+{ power *= 10; }
+f = num / power, f_power = f *power, num -= f_power;
+if (j == 0 && sign < 0)
+_putchar('-');
+_putchar('0' + f);
+}
+}
+else
+_putchar('0');
+if (i + 1 != n)
+{_putchar(','), _putchar(' '); }
+
+}
