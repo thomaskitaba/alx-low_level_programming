dfc99895050b68c36a871792a753c381c0ce640c
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-02 23:55:07 +0300

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * print__buffer - print a buffer
  * @b: pointer to buffer
@@ -7,5 +9,22 @@
  * Return: nothing
  */
  void print_buffer(char *b, int size)
-{	
+{
+	int i;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	for (i = 0; i < size;  i++)
+	{
+		if ((i != 0) && (i % 10 == 0))
+		{
+			putchar('\n');
+			printf("%p: ", &b[i]);
+		}
+		printf("%d", *(b + i));
+		if ((i + 1) % 10 != 0)
+			putchar(' '); 
+	}
 }
