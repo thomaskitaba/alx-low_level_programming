e321f390a091f99f8bfcd95a61d078c74aeb08fc
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-02 00:25:08 -0800

    7-leet

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,9 +15,9 @@ for (i = 0; i < str_len; i++)
 if ((i == 0) && (str[i] >= 97 && str[i] <= 122))
 {
 str[i] = str[i] - 32; }
-if (((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n') || \n
-(str[i] == '.') || (str[i] == ',') || (str[i] == '!') || (str[i] == '?') || \n
-(str[i] == '"') || (str[i] == '(') || (str[i] == ')') || (str[i] == '}') || \n
+if (((str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n') ||
+(str[i] == '.') || (str[i] == ',') || (str[i] == '!') || (str[i] == '?') ||
+(str[i] == '"') || (str[i] == '(') || (str[i] == ')') || (str[i] == '}') ||
 (str[i] == '}')) && (str[i + 1] != str_len))
 {
 if (str[i + 1] >= 97 && str[i + 1] <= 122)
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,20 +9,20 @@ char *leet(char *str)
 {
 int i, j, str_len, ltr_len;
 
-char letter[]= "aAeEoOtTlL";
-char number[]= "4433007711";
+char letter[] = "aAeEoOtTlL";
+char number[] = "4433007711";
 str_len = (int)strlen(str);
 ltr_len = (int)strlen(letter);
 
 for (i = 0; i < str_len; i++)
 {
-  for (j = 0; j < ltr_len; j++)
-  {
-    if (str[i] == letter[j])
-    {
-      str[i] = number[j];
-    }
-  }
+for (j = 0; j < ltr_len; j++)
+{
+if (str[i] == letter[j])
+{
+str[i] = number[j];
 }
-return (str); 
+}
+}
+return (str);
 }
