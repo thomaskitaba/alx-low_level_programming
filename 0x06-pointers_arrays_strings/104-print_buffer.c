d9a60cf82aa58eea8e693e91da1b81792a076cc3
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-02 21:43:45 +0300

    102-magic

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print__buffer - print a buffer
+ * @b: pointer to buffer
+ * @size: byte size pointed by b
+ * Return: nothing
+ */
+ void print_buffer(char *b, int size)
+{
+	
+}
