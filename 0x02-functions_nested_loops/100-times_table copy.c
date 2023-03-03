a18ce9621d013b7af2b435d2955ffe5ad58f01e1
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-03 17:16:58 +0300

    correction to 100-times_table

---
+++ 0x02-functions_nested_loops/100-times_table copy.c
@@ -0,0 +1,64 @@
+#include "main.h"
+/**
+ * print_times_table -  function that prints the 9 times table, starting with 0.
+ * @n : n times table
+ */
+void print_times_table(int n)
+{
+	int i, j, f, s, l, temp;
+	int result;
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
+				if (result > 9 && result <= 99)
+				{
+					l = result % 10;
+					f = (result - l) / 10;
+
+					_putchar('0' + f);
+					_putchar('0' + l);
+				}
+				else if (result > 99)
+				{
+					temp = result;
+					f = temp / 100;
+					s = (result - (f * 100)) / 10;
+					l = result % 10;
+					_putchar('0' + f);
+					_putchar('0' + s);
+					_putchar('0' + l);
+				}
+				else
+				{
+					_putchar('0' + result);
+				}
+				if (j != n)
+				{
+					_putchar(',');
+
+					if (result <= 9)
+					{
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+
+					else if (result > 9 && result <= 99)
+					{
+						_putchar(32);
+						_putchar(32);
+					}
+					else
+					{
+						_putchar(32);
+					}
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
