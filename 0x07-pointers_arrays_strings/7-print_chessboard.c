65fd3d4d339175a40fc44ad13499a00d800e709f
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-06 21:26:09 +0300

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,19 +4,19 @@
 /**
  * print_chessboard - Entry point for the program
  * @a: two dimensional array
- * Return: nothing 
+ * Return: nothing
  */
 
 void print_chessboard(char (*a)[8])
 {
-  int i, j;
+int i, j;
 
-  for (i = 0; i < 8; i++)
-  {
-    for (j = 0; j < 8; j++)
-    {
-      _putchar(a[i][j]);
-    }
-    _putchar('\n');
-  }
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
 }
