9d359b6deb10506b94564b891d987c1cf129624f
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-06 20:25:32 +0300

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 #include <string.h>
 /**
  * _strspn - return initial segment
@@ -10,32 +9,28 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-  int i, j , position, s_len, c_len;
-  
+int i, j , position, s_len, c_len;
 
-  s_len = (int)strlen(s);
-  c_len = (int)strlen(accept);
-  position = 0;
 
-  for (i = 0; i < s_len; i++)
-  {
-    for (j = 0; j < c_len; j++)
-    {      
-      if (s[i] == accept[j])
-      {
-        position = i + 1;
-        
-        continue;
-      }
-      
-      if (j + 1 == c_len)
-      {
-        return (position);
-        
-      }
-      
-    }
-  
-  }
-  return (position);
+s_len = (int)strlen(s);
+c_len = (int)strlen(accept);
+position = 0;
+
+for (i = 0; i < s_len; i++)
+{
+for (j = 0; j < c_len; j++)
+{
+if (s[i] == accept[j])
+{
+position = i + 1;
+break;
+}
+if (j + 1 == c_len)
+{
+return (position);
+}
+
+}
+}
+return (position);
 }
