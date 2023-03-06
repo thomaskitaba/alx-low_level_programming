13fbdcb8460023b6c81a6901836bd1a1118415c2
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-06 11:55:17 +0300

    prepare 0x07 for workout

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 on success, other values on error
+ */
+
---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 on success, other values on error
+ */
