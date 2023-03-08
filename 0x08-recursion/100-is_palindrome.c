fdcc71136194aad1fcc788af9ebe1fdfc4b8db15
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-08 20:36:37 +0300

    2-strlen_recursions

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * is_palindrome - prints recursion
  * @s: string
@@ -7,5 +8,16 @@
  */
 int is_palindrome(char *s)
 {
+	int i, s_len;
+  
+	s_len = (int)strlen(s);
+	
 
+	for (i = 0; i <= s_len / 2; i++)
+	{
+		if (s[i] != s[s_len - i - 1])
+			return (0);
+		
+	}
+	return (1);
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,9 +9,10 @@
 int _strlen_recursion(char *s)
 {
   int i;
+  i = 0;
 
   while(s != '\0')
   {
-    _strlen_recursion(s + 1);
+    s - _strlen_recursion(s + 1);
   }  
 }
