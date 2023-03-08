71e3b64baa865d9ed69d2622bd625d8a78956dcc
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-08 21:14:17 +0300

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,16 +7,15 @@
  */
 int factorial(int n)
 {
-	
-  if (n == 1 || n == 0)
-	{
-		return (1);
-	}
-  if (n > 0)
-	{
-		n *= factorial (n - 1);
-		return (n);
-	}
-	else
-	return (-1);
+if (n == 1 || n == 0)
+{
+return (1);
+}
+if (n > 0)
+{
+n *= factorial (n - 1);
+return (n);
+}
+else
+return (-1);
 }
