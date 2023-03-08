35cd9f2583ef9ccd63a05d051692dcb996cf6f29
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-08 21:09:00 +0300

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,11 @@
 int _pow_recursion(int x, int y)
 {
 int power;
-
 if (y < 0)
-  return (-1);
+return (-1);
 if (y == 0)
-  return (1);
-
+return (1);
 if (y > 0)
-  
-  power = x * _pow_recursion(x, y - 1);
-
+power = x * _pow_recursion(x, y - 1);
 return (power);
 }
