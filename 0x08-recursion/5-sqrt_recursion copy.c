c7253b61f7c1835bbd36ec7127f1f357ebbb1d1f
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-09 14:43:59 +0300

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion copy.c
+++ 0x08-recursion/5-sqrt_recursion copy.c
@@ -8,15 +8,15 @@
  */
 int _square(int n, int i)
 {
-if (i <= n / 2)
-{
-if ( n == i * i)
-{
-return (i);
-}
-return (_square(n, i + 1));
-}
-return (-1);
+  if (i <= n / 2)
+  {
+    if ( n == i * i)
+    {
+      return (i);
+    }
+    return (_square(n, i + 1));
+  }
+  return (-1);
 }
 /**
  * _sqrt_recursion - _sqrt_recursion
@@ -27,9 +27,10 @@ return (-1);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-return (-1);
+  return (-1);
 else
 {
-return (_square(n, 1));
+  return (_square(n, 1));
 }
+
 }
