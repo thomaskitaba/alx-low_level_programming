77a356df09a0d964a29997dd75a5699e536abf7b
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-10 14:19:39 +0300

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,7 +22,7 @@ for (i = 1; i < argc; i++)
 {
 for (j = 0; j < (int)strlen(argv[i]); j++)
 {
-if ( argv[i][j] < '0' || argv[i][j] > '9')
+if (argv[i][j] < '0' || argv[i][j] > '9')
 {
 printf("%s\n", "Error");
 return (1);
