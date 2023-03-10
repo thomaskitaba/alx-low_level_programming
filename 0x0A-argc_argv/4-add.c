6b84c1f10d2256c7865b598a87eb4acb7eccee40
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-10 14:17:15 +0300

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,21 +20,20 @@ if (argc > 1)
 /*check if it is number*/
 for (i = 1; i < argc; i++)
 {
-  for (j = 0; j < (int)strlen(argv[i]); j++)
-  {
-    if ( argv[i][j] < '0' || argv[i][j] > '9')
-    {
-      printf("%s\n", "Error");
-      return (1);
-    }
-    if (argv[i][j + 1] == '\0')
-    result += atoi(argv[i]);
-  }
+for (j = 0; j < (int)strlen(argv[i]); j++)
+{
+if ( argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("%s\n", "Error");
+return (1);
+}
+if (argv[i][j + 1] == '\0')
+result += atoi(argv[i]);
+}
 }
 printf("%d\n", result);
 return (0);
 }
-
 printf("%d\n", 0);
 return (0);
 }
