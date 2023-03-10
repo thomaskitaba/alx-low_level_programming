97e935c594259fad9c814c17de33b89bc1a2ab61
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-10 12:57:40 +0300

    ./mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point for the program
+ * @argc: argument count
+ * @argv: argument vectory/array
+ *
+ * Return: 0 on success, other integer on error
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s", argv[0]);
+return (0);
+}
