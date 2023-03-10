4b0f36be2718ea48e817c6907acfd1d99d2d7fee
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-10 13:21:46 +0300

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,6 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-printf("%d\n", argc);
+
+if((int)strlen(argv[0]) == 0)
+{
+  printf("%d\n", 0);
+  return (0);
+}
+printf("%d\n", argc - 1);
+
 return (0);
 }
