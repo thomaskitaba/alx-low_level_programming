0ad7e63c3888cb97d7f426289178d2895a5d92d6
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-10 13:09:40 +0300

    1-arg.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point for the program
+ * @argc: argument count
+ * @argv: argument vectory/array
+ *
+ * Return: 0 on success, other integer on error
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
