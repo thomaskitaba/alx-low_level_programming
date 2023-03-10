c8f0a3f80226044fafafa45d711230eae1eb4a58
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-10 13:52:06 +0300

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,13 +13,13 @@
 int main(int argc, char *argv[])
 {
 int result;
-
-if (argc != 3)
+if (argc == 3)
 {
-  printf("%s\n", "Error");
-  return (1);
-}
 result = atoi(argv[1]) * atoi(argv[2]);
-printf("%d/n", result);
+printf("%d\n", result);
 return (0);
+}
+
+printf("%s\n", "Error");
+return (1);
 }
