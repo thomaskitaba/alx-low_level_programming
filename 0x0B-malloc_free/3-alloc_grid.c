93972509e97601fa36fa1bc32812f386dc73a67f
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-13 16:01:00 +0300

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * alloc_grid -  writes the character c to stdout
@@ -9,5 +9,18 @@
  */
 int **alloc_grid(int width, int height)
 {
+int i, n;
+int **temp;
 
+if (width <=0 || height <= 0)
+	return (NULL);
+temp = malloc(width * height);
+
+if (temp == NULL)
+	return (NULL);
+for (i = 0, n = width * height; i < n; i++)
+{
+	*(temp + i) = 0;
+}
+return (temp);
 }
