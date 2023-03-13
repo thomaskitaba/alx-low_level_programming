82d0f8640bda876ba1d32facaf09a7ddd4ec5bdd
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-13 19:08:02 +0300

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,19 +12,19 @@ int **alloc_grid(int width, int height)
 int i, j;
 int **temp;
 
-if (width <=0 || height <= 0)
+if (width <= 0 || height <= 0)
 return (NULL);
 
 temp = (int **)malloc(height * sizeof(int *));
 
 if (temp == NULL)
 return (NULL);
-for (i = 0; i < height; i ++)
+for (i = 0; i < height; i++)
 {
 temp[i] = (int *)malloc(width * sizeof(int));
 for (j = 0; j < width; j++)
 {
-temp[i][j]= 0;
+temp[i][j] = 0;
 }
 }
 return (temp);
