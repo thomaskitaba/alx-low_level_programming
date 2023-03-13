37e56bb54c4f86e3f03840fd64f0822bd948820a
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-13 23:24:04 +0300

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
+/**
+ * _memcpy - Entry point for the program
+ * @n: number of bytes to be copied
+ * @src: src to be copied
+ * @dest: destinatio memory location
+ * Return: pointer to destination
+ */
 
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i;
+
+for (i = 0; i < (int)n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
 /**
  * argstostr -  turn arguments into string
  * @ac: The character to print
@@ -10,32 +27,36 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, k, av_len;
+	int i, k, n;
+	int av_len;
 	char *temp;
 
+	
 	av_len = 0;
 	if (ac == 0 || av == NULL)
 		return NULL;
 
 	for (k = 0; k < ac; k++)
 	{
-		av_len += (int)strlen(av[k]);
+
+		for (n = 0; n < (int)strlen(av[k]); n++)
+		{
+			av_len++;
+		}
 	}
 
+	av_len = av_len + 1 + ac; /* null char and spaces*/
 	temp = (char *)malloc(av_len);
 
 	if (temp == NULL)
 		return (NULL);
 	
-
-	for (i = 0; i < av_len; i++)
+	for (i = 0; i < ac; i++)
 	{
-		if (*av[i] != ' ')
-			temp[i] = *(av[0] + i);
-		else
-			temp[i] = '\n';
-		}
+		
+		_memcpy(temp, av[i], (unsigned int)strlen(av[i]));
+		
+	}
 	
-
 	return (temp);
 }
