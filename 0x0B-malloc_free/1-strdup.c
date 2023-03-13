0b3c83af2a714a01d6cbf08ea85a5f31e1cdd74d
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-13 14:42:33 +0300

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,11 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
+#include <strlen.h>
 /**
  * _strdup -  writes the character c to stdout
  * @str: The character to print
  * Return: return 0 on success
  */
 char *_strdup(char *str)
-{
+{ 
+	char *duplicate;
+	int i, str_len;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	str_len = (int)strlen(str);
+	duplicate = malloc(sizeof(str));
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < str_len; i++)
+	{
+		*(duplicate + i) = *(str + i);
+	}
+
+	return (duplicate);
 }
