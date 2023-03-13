1aac7ad38a9504a8a6d894fb624eaaef461bf8f7
thomaskitaba <thomas.kitaba@gmail.com>
2023-03-13 20:20:02 +0300

    3-alloc_grid.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,13 +1,35 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * argstostr -  turn arguments into string
  * @ac: The character to print
  * @av: pointr
- * Return: return char 
+ * Return: return char
  */
 char *argstostr(int ac, char **av)
 {
+	int i, j;
+	char *temp;
+
+
+	if (ac == 0 || av == NULL)
+		return NULL;
+	for (i = 0; i < ac; i++)
+	{
+		temp = (char *)malloc(sizeof((int)strlen(av[i])) + 4);
+		if (temp == NULL)
+			return NULL;
+		for (j = 0; j <= (int)strlen(av[i]) ; j++)
+		{
+			temp[i + j] = av[i][j];
+			if (j + 1 = (int)strlen(av[i]))
+				temp[i][j] = '\n';
+
+		}
+		
+	}
+	return (temp);
 
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,6 +22,12 @@ return (NULL);
 for (i = 0; i < height; i++)
 {
 temp[i] = (int *)malloc(width * sizeof(int));
+if (temp[i] == NULL)
+{
+  free(temp[i]);
+  free(temp);
+  return (NULL);
+}
 for (j = 0; j < width; j++)
 {
 temp[i][j] = 0;
